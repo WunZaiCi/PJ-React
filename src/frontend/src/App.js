4c42c0f07a9dcc8768af35295b5f1f3d0d2d20cf
Liu Weijie <lwj34158427@gmail.com>
2024-11-21 19:02:02 +0900

    Initial commit

---
+++ src/frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import  {Button, Radio} from "antd";
+import {getAllStudents} from "./client";
+
+function App() {
+    getAllStudents()
+        .then(res => res.json())
+        .then(console.log)
+  return (
+    <div className="App">
+      <Button type={"primary"}>Hello</Button>
+      <br/>
+      <Radio.Group value='large'>
+        <Radio.Button value="Large">Large</Radio.Button>
+        <Radio.Button value="default">Default</Radio.Button>
+        <Radio.Button value="small">Small</Radio.Button>
+      </Radio.Group>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/java/com/liuweijie/demo/student/Gender.java
@@ -0,0 +1,7 @@
+package com.liuweijie.demo.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
